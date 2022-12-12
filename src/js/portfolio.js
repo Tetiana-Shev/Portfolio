54877ce30494041fff402c9ea2ca68f33eacc40c
Tetiana-Shev <azaliya939@gmail.com>
2022-12-12 17:34:45 +0200

    Add info

---
+++ src/js/portfolio.js
@@ -0,0 +1,10 @@
+import Splide from '@splidejs/splide';
+
+new Splide('#slider1', {
+  type: 'loop',
+  autoWidth: true,
+  focus: 'center',
+  gap: '1em',
+  pagination: false,
+  keyboard: true,
+}).mount();
